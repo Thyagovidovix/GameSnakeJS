d056702dfc13e45cdf9a672fed82a47838a6f7b9
Thyagovidovix <thyagovidovix@gmail.com>
2020-08-29 15:59:09 -0300

    add body stylization

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function criarBG() {
 function criarCobrinha() {
     for (i = 0; i < snake.length; i++) {
         context.fillStyle = "green";
-        context.fillRect(snake[i].x, snake[i].y, box, box);
+        context.fillRect(snake[i].x, snake[i].y, box - 1, box - 1);
     }
 }
 
